c656001181aeb4a27eda660de38e05d7eb94ff99
Etzion Harari <etzionhar@gmail.com>
2020-10-12 19:12:14 +0300

    Add files via upload

---
+++ implementation.py
@@ -0,0 +1,30 @@
+from cumulative_heatmap import HeatMap, loadshp, loadcsv, loadkml
+import pandas as pd
+import numpy as np
+
+
+# Example 1:
+xy = loadcsv(r'examples\random.csv','x','y')
+hm = HeatMap(xy, 25)
+hm.save_map(r'output\random_output','csv')
+hm.plot()
+
+# Example 2:
+xy = loadshp(r'examples\paris.shp')
+hm = HeatMap(xy, 80)
+hm.save_map(r'output\paris_output','kml')
+hm.plot()
+
+# Example 3:
+xy = loadkml(r'examples\Athens.kml')
+hm = HeatMap(xy, 64)
+hm.save_map(r'output\Athens_output','shp')
+hm.plot()
+
+# Example 4:
+xy = np.load(r'examples\data.npy')
+hm = HeatMap(xy, 128)
+hm.save_map(r'output\data_output','csv')
+hm.plot()
+
+
